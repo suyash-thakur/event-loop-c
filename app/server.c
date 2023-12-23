168aa4acd03cb9acaca7d419393103cf8f8dce35
suyash-thakur <500067151@stu.upes.ac.in>
2023-12-23 21:02:05 +0530

    even better event loop 😃

--- app/server.c
+++ app/server.c
@@ -7,120 +7,32 @@
 #include <errno.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <fcntl.h>
 
 #define MAX_THREADS 5
 
 typedef struct
 {
 	pthread_t thread;
-	int is_active;
 	int client_fd;
 	struct sockaddr_in client_addr;
-	int client_addr_len;
 } ThreadInfo;
 
-void *handle_client(void *arg)
+typedef struct
 {
-	printf("Handling client\n");
-	ThreadInfo *thread_info = (ThreadInfo *)arg;
-	int client_fd = thread_info->client_fd;
+	ThreadInfo *threads;
+	int count;
+	int capacity;
+	pthread_mutex_t mutex;
+} ThreadPool;
 
-	int is_connected = 1;
-
-	while (is_connected)
-	{
-		char request[1024];
-		printf("Waiting for a request...\n");
-		int byte_read = read(client_fd, request, sizeof(request) - 1);
-
-		printf("byte_read: %d\n", byte_read);
-		if (byte_read <= 0)
-		{
-			is_connected = 0;
-			break;
-		}
-		request[byte_read] = '\0';
-
-		printf("Request: %s\n", request);
-
-		char response[1024] = "+PONG\r\n";
-		write(client_fd, response, strlen(response));
-	}
-
-	printf("Client disconnected\n");
-	close(client_fd);
-	return NULL;
-}
-
-void *event_loop(void *arg)
-{
-	int server_fd = *((int *)arg);
-	fd_set read_fds;
-	int max_fd = server_fd;
-
-	ThreadInfo thread_info[MAX_THREADS] = {0};
-
-	while (1)
-	{
-		FD_ZERO(&read_fds);
-		FD_SET(server_fd, &read_fds);
-
-		for (int i = 0; i < MAX_THREADS; i++)
-		{
-			if (thread_info[i].is_active)
-			{
-				FD_SET(thread_info[i].client_fd, &read_fds);
-				if (thread_info[i].client_fd > max_fd)
-				{
-					max_fd = thread_info[i].client_fd;
-				}
-			}
-		}
-
-		int activity = select(max_fd + 1, &read_fds, NULL, NULL, NULL);
-		if ((activity < 0) && (errno != EINTR))
-		{
-			perror("select");
-			exit(EXIT_FAILURE);
-		}
-
-		if (FD_ISSET(server_fd, &read_fds))
-		{
-			socklen_t addr_len = sizeof(thread_info[0].client_addr);
-			int client_fd = accept(server_fd, (struct sockaddr *)&thread_info[0].client_addr, &addr_len);
-			if (client_fd < 0)
-			{
-				perror("accept");
-				exit(EXIT_FAILURE);
-			}
-
-			for (int i = 0; i < MAX_THREADS; i++)
-			{
-				if (!thread_info[i].is_active)
-				{
-					thread_info[i].is_active = 1;
-					thread_info[i].client_fd = client_fd;
-					thread_info[i].client_addr = thread_info[0].client_addr;
-					thread_info[i].client_addr_len = sizeof(thread_info[0].client_addr);
-					printf("New connection, assigned to thread %d\n", i);
-					pthread_create(&thread_info[i].thread, NULL, handle_client, &thread_info[i]);
-					break;
-				}
-			}
-		}
-
-		for (int i = 0; i < MAX_THREADS; i++)
-		{
-			if (thread_info[i].is_active && FD_ISSET(thread_info[i].client_fd, &read_fds))
-			{
-				pthread_join(thread_info[i].thread, NULL); // Wait for thread to finish
-				printf("Handling client in thread %d completed\n", i);
-				thread_info[i].is_active = 0;
-				thread_info[i].client_fd = 0;
-			}
-		}
-	}
-}
+void *handle_client(void *arg);
+void *event_loop(void *arg);
+void initialize_thread_pool(ThreadPool *pool, int capacity);
+void cleanup_thread_pool(ThreadPool *pool);
+void handle_new_connections(int server_fd, ThreadPool *thread_pool);
+fd_set master_set;
+int max_fd;
 
 int main()
 {
@@ -155,6 +67,8 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 
+	max_fd = server_fd;
+
 	int connection_backlog = 5;
 	if (listen(server_fd, connection_backlog) != 0)
 	{
@@ -162,12 +76,140 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 
+	// Create a thread pool and start the event loop
 	pthread_t event_loop_thread;
 	pthread_create(&event_loop_thread, NULL, event_loop, &server_fd);
 
+	// Wait for the event loop thread to complete
 	pthread_join(event_loop_thread, NULL);
 
+	// Close the server socket
 	close(server_fd);
 
 	return 0;
+}
+
+void *handle_client(void *arg)
+{
+	ThreadInfo *thread_info = (ThreadInfo *)arg;
+	int client_fd = thread_info->client_fd;
+
+	char buffer[1024];
+	int bytes_read;
+
+	while (1)
+	{
+
+		bytes_read = recv(client_fd, buffer, sizeof(buffer), 0);
+		if (bytes_read <= 0)
+		{
+			// Connection closed or error
+			break;
+		}
+
+		char *response = "+OK\r\n";
+
+		send(client_fd, response, strlen(response), 0);
+	}
+
+	close(client_fd);
+	return NULL;
+}
+
+void *event_loop(void *arg)
+{
+	int server_fd = *((int *)arg);
+	ThreadPool thread_pool;
+	initialize_thread_pool(&thread_pool, MAX_THREADS);
+
+	FD_ZERO(&master_set);
+	FD_SET(server_fd, &master_set);
+
+	while (1)
+	{
+		fd_set read_fds = master_set;
+
+		int activity = select(max_fd + 1, &read_fds, NULL, NULL, NULL);
+		if (activity == -1)
+		{
+			perror("select");
+			exit(EXIT_FAILURE);
+		}
+
+		// Handle new connections
+		if (FD_ISSET(server_fd, &read_fds))
+		{
+			handle_new_connections(server_fd, &thread_pool);
+		}
+
+		// Handle data on existing connections
+		for (int i = 0; i < thread_pool.count; i++)
+		{
+			int client_fd = thread_pool.threads[i].client_fd;
+			if (FD_ISSET(client_fd, &read_fds))
+			{
+				// Create a new thread to handle the client
+				pthread_create(&thread_pool.threads[i].thread, NULL, handle_client, &thread_pool.threads[i]);
+			}
+		}
+
+		// Clean up finished threads
+		for (int i = 0; i < thread_pool.count; i++)
+		{
+			pthread_join(thread_pool.threads[i].thread, NULL);
+		}
+
+		// Separate loop for cleaning up closed connections
+		for (int i = 0; i < thread_pool.count; i++)
+		{
+			int client_fd = thread_pool.threads[i].client_fd;
+			if (FD_ISSET(client_fd, &read_fds))
+			{
+				close(client_fd);
+				FD_CLR(client_fd, &master_set);
+			}
+		}
+	}
+
+	cleanup_thread_pool(&thread_pool);
+	return NULL;
+}
+
+void initialize_thread_pool(ThreadPool *pool, int capacity)
+{
+	printf("Initializing thread pool with capacity %d\n", capacity);
+	pool->threads = (ThreadInfo *)malloc(sizeof(ThreadInfo) * capacity);
+	pool->count = 0;
+	pool->capacity = capacity;
+	pthread_mutex_init(&pool->mutex, NULL);
+}
+
+void cleanup_thread_pool(ThreadPool *pool)
+{
+	free(pool->threads);
+	pthread_mutex_destroy(&pool->mutex);
+}
+
+void handle_new_connections(int server_fd, ThreadPool *thread_pool)
+{
+	int client_fd = accept(server_fd, NULL, NULL);
+	if (client_fd > 0)
+	{
+		// Add the new client socket to the master set
+		FD_SET(client_fd, &master_set);
+
+		// Update max_fd if needed
+		if (client_fd > max_fd)
+		{
+			max_fd = client_fd;
+		}
+
+		// Add the new client socket to the thread pool
+		pthread_mutex_lock(&thread_pool->mutex);
+		thread_pool->threads[thread_pool->count].client_fd = client_fd;
+		pthread_mutex_unlock(&thread_pool->mutex);
+
+		// Increment the thread pool count
+		thread_pool->count++;
+	}
 }
